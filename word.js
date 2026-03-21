c4130bae433805a9cc5bc7f9172164f83999cee2
ShivaPrasadAcharya <shivaprasadacharya000000000000@gmail.com>
2026-03-21 22:43:17 +0545

    Add files via upload

---
+++ word.js
@@ -0,0 +1,49 @@
+const wordData = [
+    {
+        "id": "",
+        "Verse": "9.1",
+        "Word": "अंश",
+        "Trans": "a.nsha",
+        "Sa": "भागः, अवयवः",
+        "Ne": "भाग, अंश",
+
+    },
+    {
+        "id": "2.7.1",
+        "Trans": "a.nshaH",
+        "Sa": "भागः, कणः, अवयवः",
+        "Ne": "सानो भाग, कण",
+        "En": "fragmental particle",
+        "Hi": "छोटा भाग, कण",
+        "chinese": "ishang "
+    },
+    {
+        "id": "7.5",
+        "Verse": "5.29",
+        "Word": "शिव",
+        "Trans": "shiva",
+        "Sa": "कल्याण",
+        "Ne": "करूणा",
+        "En": "supreme consciousness",
+        "Hi": "उच्चतम ज्ञान"  },
+    {
+        "id": "2.7.1",
+        "Verse": "2.5",
+        "Word": "अंशः",
+        "Trans": "a.nshaH",
+        "Sa": "भागः, कणः, अवयवः",
+        "Ne": "सानो भाग, कण",
+        "En": "fragmental particle",
+        "Hi": "छोटा भाग, कण"
+    },
+    {
+        "id": "2",
+        "Verse": "3.15",
+        "Word": "अंशुमान्",
+        "Trans": "a.nshumAn",
+        "Sa": "तेजस्वी, रश्मिमान्, सूर्यः",
+        "Ne": "तेजस्वी, किरणमय",
+        "En": "radiant",
+        "Hi": "तेजस्वी, किरणों वाला"
+    }
+];
